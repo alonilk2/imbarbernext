b514090e67d1ecfaf8711cde990248ce78878474
Alon Barenboim <alonilk2@gmail.com>
2025-04-17 21:44:38 +0300

    dev

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: '/imbarbernext',
-
+  assetPrefix: process.env.NODE_ENV === "production" ? "/imbarbernext/" : undefined,
 };
 
 export default nextConfig;
